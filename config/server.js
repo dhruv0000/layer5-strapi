cd71d10d9d1c3d0be7b9691ffea2b39ef97bb18c
dhruv0000 <patel.4@iitj.ac.in>
2020-08-14 15:37:32 +0530

    Inital Commit
    Signed-off-by: dhruv0000 <patel.4@iitj.ac.in>

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f7aefe748fd6a445897bbc24494cffbd'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a58f7a6f-bff0-4b7f-ae97-cb84f8ec1e6a'
+};
